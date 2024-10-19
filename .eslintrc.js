adb708379cce3e818ed529199ec9a5d8de4b742f
jetie000 <makeenko.005@gmail.com>
2024-10-20 01:56:55 +0300

    feat: add tests for api

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,4 +21,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
+  include: ['src/**/*.ts', 'test/**/*.ts'],
 };
